77d1e25e6096aa6eb9c4c094329a903cc946dbd1
sainrahmanii <husainrahmani127@gmail.com>
2023-04-02 20:51:20 +0700

    first commit | dart-library

---
+++ example/hello.dart
@@ -0,0 +1,9 @@
+import 'package:dart_library/hello.dart';
+
+void main(){
+  var customer = Customer();
+  var product = Product();
+
+  print(sayHello('Husain Rahmani'));
+
+}
---
+++ lib/hello.dart
@@ -0,0 +1,4 @@
+library dart_library;
+
+export 'src/customer.dart' show Customer, Product;
+export 'src/say_hello.dart';
